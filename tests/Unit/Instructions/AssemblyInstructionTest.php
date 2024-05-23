shanept/AssemblySimulator

commit e7edf94cb3dab32389242cf3c9f684826545869a

Added missing SIB parsing.

src/Instruction/AssemblyInstruction.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace shanept\AssemblySimulator\Instruction;
 
+use shanept\AssemblySimulator\Register;
 use shanept\AssemblySimulator\Simulator;
 use shanept\AssemblySimulator\Address\SibAddress;
 use shanept\AssemblySimulator\Address\RipAddress;
@@ -109,6 +110,58 @@ abstract class AssemblyInstruction
         ];
     }
 
+    protected function parseSibByte($byte)
+    {
+        $sim = $this->getSimulator();
+
+        $byte = ord($byte);
+        $sib = [
+            's' => ($byte & 0b11000000) >> 6,
+            'i' => ($byte & 0b00111000) >> 3,
+            'b' => $byte & 0b00000111,
+        ];
+
+        $opSize = $this->getOperandSize();
+
+        $rex = $sim->getRex();
+        $rexSet = (bool) ($rex & Simulator::REX);
+        $idxExt = (bool) ($rex & Simulator::REX_X);
+        $bseExt = (bool) ($rex & Simulator::REX_B);
+
+        /**
+         * Scale represents the multiplier for the index.
+         *
+         * If scale bit == 0b00, scale is 1.
+         * If scale bit == 0b01, scale is 2.
+         * If scale bit == 0b10, scale is 4.
+         * If scale bit == 0b11, scale is 8.
+         */
+        $scale = 2 ** $sib['s'];
+
+        /**
+         * Index and Base both refer to registers, in the same fashion as
+         * ModRM bytes.
+         *
+         * If the value of Index is 4, we do not apply a scaled index. Thus we
+         * will override the index to 0, making the index scale to 0 as well.
+         */
+        $index = 0;
+
+        if (0x4 !== $sib['i']) {
+            $reg = Register::getByCode($sib['i'], $opSize, $rexSet, $idxExt);
+            $index = $sim->readRegister($reg);
+        }
+
+        $reg = Register::getByCode($sib['b'], $opSize, $rexSet, $bseExt);
+        $base = $sim->readRegister($reg);
+
+        return [
+            's' => $scale,
+            'i' => $index,
+            'b' => $base,
+        ];
+    }
+
     protected function unpackImmediate($immediate, $size)
     {
         $packs = [
@@ -137,6 +190,9 @@ abstract class AssemblyInstruction
          * If the r/m byte is 0x5, we have RIP addressing.
          */
         if (0x4 === $byte["rm"]) {
+            $sibByte = $sim->getCodeAtInstruction(1);
+            $sib = $this->parseSibByte($sibByte);
+
             /**
              * Calculate the displacement of the SIB operation. The offset
              * is specified by the ModRM mod byte:
@@ -145,17 +201,16 @@ abstract class AssemblyInstruction
              * If mod = 0b01, displacement is 1.
              * If mod = 0b10, displacement is 4.
              */
-            $disp = 2 == $byte["mod"] ? 4 : $byte["mod"];
+            $dispSize = 2 == $byte["mod"] ? 4 : $byte["mod"];
+            $displacement = $sim->getCodeBuffer(1, $dispSize);
 
-            $sibByte = $sim->getCodeAtInstruction(1);
+            $instructionPointer = $sim->getInstructionPointer();
 
             $address = new SibAddress(
-                $sim->getRex(),
-                $sim->getPrefix(),
-                $sim->getRawRegisters(),
-                $sibByte,
-                $disp,
-                0,
+                $instructionPointer,
+                $sib,
+                $displacement,
+                $dispSize + 1,
             );
 
             return $address;

tests/Unit/Instructions/MovTest.php
@@ -152,7 +152,6 @@ class MovTest extends \PHPUnit\Framework\TestCase
                   ->willReturn(0);
 
         $values = [
-            "\x10",
             "\x24",
             "\x74",
         ];
@@ -162,9 +161,21 @@ class MovTest extends \PHPUnit\Framework\TestCase
                       return array_pop($values);
                   });
 
+        $simulator->method('getCodeBuffer')
+                  ->with(1, 1)
+                  ->willReturn("\x10");
+
         $simulator->method('readRegister')
-                  ->with(Register::RSI)
-                  ->willReturn(690);
+                  ->willReturnCallback(function ($register) {
+                      switch ($register) {
+                          case Register::RSI:
+                              return 690;
+                          case Register::RSP:
+                              return 0;
+                      }
+
+                      $this->fail(sprintf('Unexpected register %s.', $register['name']));
+                  });
 
         $simulator->expects($this->exactly(3))
                   ->method('advanceInstructionPointer');

commit 0f6161a8c16354cf20035510a2eaccd103a88de7

More SIB addressing improvements.
Adds support for SIB 32-bit displacement only mode. Also fixes a
bug introduced in commit e7edf94. Where SIB index 4 indicates no
displacement, this is only true if the rex extended index is not
set. If it is set, it should behave as normal, this commit allows
for that.

src/Instruction/AssemblyInstruction.php
@@ -116,13 +116,14 @@ abstract class AssemblyInstruction
      * Parses a specific SIB byte, resolving values from the registers, and
      * returning the values as an array,
      *
-     * @see
+     * @see https://wiki.osdev.org/X86-64_Instruction_Encoding#SIB
      *
-     * @param string $byte The byte to parse.
+     * @param string $byte  The byte to parse.
+     * @param array  $modrm The ModRM byte (optional).
      *
      * @return int[] The SIB byte values.
      */
-    protected function parseSibByte(string $byte): array
+    protected function parseSibByte(string $byte, array $modrm): array
     {
         $sim = $this->simulator;
 
@@ -133,6 +134,7 @@ abstract class AssemblyInstruction
             'b' => $byte & 0b00000111,
         ];
 
+        $mode = $sim->getMode();
         $opSize = $this->getOperandSize();
 
         $rex = $sim->getRex();
@@ -154,23 +156,55 @@ abstract class AssemblyInstruction
          * Index and Base both refer to registers, in the same fashion as
          * ModRM bytes.
          *
-         * If the value of Index is 4, we do not apply a scaled index. Thus we
-         * will override the index to 0, making the index scale to 0 as well.
+         * If the value of Index is 4 and the REX.X extended index bit is unset,
+         * we do not apply a scaled index. Thus we will override the index to 0,
+         * making the index scale to 0 as well. If the REX.X bit is set, the
+         * above no longer applies.
          */
         $index = 0;
 
-        if (0x4 !== $sib['i']) {
+        if (0x4 !== $sib['i'] || $idxExt) {
             $reg = Register::getByCode($sib['i'], $opSize, $rexSet, $idxExt);
             $index = $sim->readRegister($reg);
         }
 
-        $reg = Register::getByCode($sib['b'], $opSize, $rexSet, $bseExt);
-        $base = $sim->readRegister($reg);
+        // Default to the ModRM displacement.
+        $displacement = 2 == $modrm["mod"] ? 4 : $modrm["mod"];
+
+        /**
+         * If the SIB base is 5, we are handling a special case displacement.
+         * Otherwise, we simply read from the registers.
+         *
+         * @see http://ref.x86asm.net/coder64.html#sib64_base_101
+         * @see http://www.c-jump.com/CIS77/CPU/x86/X77_0090_addressing_modes.htm
+         */
+        if (
+            0x5 === $sib['b'] &&
+            0b00 === $modrm['mod'] &&
+            Simulator::REAL_MODE !== $mode
+        ) {
+            // Override displacement (disp32 only).
+            $base = 0;
+            $displacement = 4;
+        } elseif (
+            0x5 === $sib['b'] &&
+            0b01 === $modrm['mod'] &&
+            Simulator::REAL_MODE !== $mode
+        ) {
+            // Override displacement - EBP + disp8 (however we do not actually displace 8 bits)
+            $displacement = 0;
+            $reg = Register::getByCode($sib['b'], $opSize, $rexSet, $bseExt);
+            $base = $sim->readRegister($reg);
+        } else {
+            $reg = Register::getByCode($sib['b'], $opSize, $rexSet, $bseExt);
+            $base = $sim->readRegister($reg);
+        }
 
         return [
             's' => $scale,
             'i' => $index,
             'b' => $base,
+            'displacement' => $displacement,
         ];
     }
 
@@ -237,7 +271,7 @@ abstract class AssemblyInstruction
         $sim = $this->simulator;
 
         $sibByte = $sim->getCodeAtInstruction(1);
-        $sib = $this->parseSibByte($sibByte);
+        $sib = $this->parseSibByte($sibByte, $byte);
 
         /**
          * Calculate the displacement of the SIB operation. The offset
@@ -247,16 +281,16 @@ abstract class AssemblyInstruction
          * If mod = 0b01, displacement is 1.
          * If mod = 0b10, displacement is 4.
          */
-        $dispSize = 2 == $byte["mod"] ? 4 : $byte["mod"];
-        $displacement = $sim->getCodeBuffer(1, $dispSize);
+        $dispSize = $sib['displacement'];
+        $instructionPointer = $sim->getInstructionPointer();
 
         if ($dispSize) {
+            $dispOffset = $instructionPointer + 1;
+            $displacement = $sim->getCodeBuffer($dispOffset, $dispSize);
             $displacement = $this->unpackImmediate($displacement, $dispSize * 8);
         } else {
             $displacement = 0;
         }
-
-        $instructionPointer = $sim->getInstructionPointer();
 
         $address = new SibAddress(
             $instructionPointer,

tests/Unit/Instructions/AssemblyInstructionTest.php
@@ -3,6 +3,7 @@
 namespace shanept\AssemblySimulatorTests\Unit\Instructions;
 
 use ReflectionMethod;
+use shanept\AssemblySimulator\Register;
 use shanept\AssemblySimulator\Simulator;
 use shanept\AssemblySimulatorTests\Fakes\TestAssemblyInstruction;
 
@@ -46,8 +47,6 @@ class AssemblyInstructionTest extends \PHPUnit\Framework\TestCase
 
     public function testParseAddressAcceptsRipAddressOnLongMode()
     {
-        $this->expectNotToPerformAssertions();
-
         $simulator = $this->getMockSimulator(Simulator::LONG_MODE);
 
         $simulator->method('getCodeAtInstruction')
@@ -68,13 +67,12 @@ class AssemblyInstructionTest extends \PHPUnit\Framework\TestCase
             "rm" => 0b101,
         ];
 
-        $parseAddress->invoke($instruction, $byte);
+        $address = $parseAddress->invoke($instruction, $byte);
+        $this->assertEquals(0xF1917E5, $address->getAddress());
     }
 
     public function testParseAddressAcceptsSibAddressOnProtectedMode()
     {
-        $this->expectNotToPerformAssertions();
-
         $simulator = $this->getMockSimulator(Simulator::PROTECTED_MODE);
 
         // mov [eax+ebx*4],ecx
@@ -88,23 +86,21 @@ class AssemblyInstructionTest extends \PHPUnit\Framework\TestCase
         $simulator->method('getInstructionPointer')
                   ->willReturn(1);
 
-        $simulator->method('getRawRegisters')
-                  ->willReturn(array_fill(0, 8, 0));
+        $simulator->method('readRegister')
+                  ->willReturnCallback(function ($register) {
+                      switch ($register) {
+                          case Register::EAX:
+                              return 54923;
+                          case Register::EBX:
+                              return 426;
+                      }
 
-        $values = [
-            "\x98",
-            "\x0C",
-        ];
+                      $this->fail('Incorrect register ' . $register['name']);
+                  });
 
         $simulator->method('getCodeAtInstruction')
                   ->with(1)
-                  ->willReturnCallback(function ($length) use (&$values) {
-                      return array_pop($values);
-                  });
-
-        $simulator->method('getCodeBuffer')
-                  ->willReturn("")
-                  ->with(1, 0);
+                  ->willReturn("\x98");
 
         $instruction = new TestAssemblyInstruction();
         $instruction->setSimulator($simulator);
@@ -117,6 +113,130 @@ class AssemblyInstructionTest extends \PHPUnit\Framework\TestCase
             "rm" => 0b100,
         ];
 
-        $parseAddress->invoke($instruction, $byte);
+        $address = $parseAddress->invoke($instruction, $byte);
+        $this->assertEquals(56629, $address->getAddress());
+    }
+
+    public function testParseAddressAcceptsSibAddressWithExtendedScaleOnLongMode()
+    {
+        $simulator = $this->getMockSimulator(Simulator::LONG_MODE);
+
+        // mov [rax+rbx*4],ecx
+        // 0x89 0x0C 0xA0
+        $simulator->method('getRex')
+                  ->willReturn(0x4A);
+
+        $simulator->method('getPrefix')
+                  ->willReturn(0);
+
+        $simulator->method('getInstructionPointer')
+                  ->willReturn(1);
+
+        $simulator->method('readRegister')
+                  ->willReturnCallback(function ($register) {
+                      switch ($register) {
+                          case Register::RAX:
+                              return 54925;
+                          case Register::R12:
+                              return 428;
+                      }
+
+                      $this->fail('Incorrect register ' . $register['name']);
+                  });
+
+        $simulator->method('getCodeAtInstruction')
+                  ->with(1)
+                  ->willReturn("\xA0");
+
+        $instruction = new TestAssemblyInstruction();
+        $instruction->setSimulator($simulator);
+
+        $parseAddress = new ReflectionMethod($instruction, "parseAddress");
+
+        $byte = [
+            "mod" => 0,
+            "reg" => 0b1,
+            "rm" => 0b100,
+        ];
+
+        $address = $parseAddress->invoke($instruction, $byte);
+        $this->assertEquals(56639, $address->getAddress());
+    }
+
+    public function testParseAddressWithSibDisp32OverrideInProtectedMode()
+    {
+        $simulator = $this->getMockSimulator(Simulator::PROTECTED_MODE);
+
+        $simulator->method('getRex')
+                  ->willReturn(0);
+
+        $simulator->method('getPrefix')
+                  ->willReturn(0);
+
+        $simulator->method('getInstructionPointer')
+                  ->willReturn(3);
+
+        $simulator->method('getCodeAtInstruction')
+                  ->willReturn("\x25");
+
+        $simulator->expects($this->once())
+                  ->method('getCodeBuffer')
+                  ->willReturn("\x28\x00\x00\x00")
+                  ->with(4, 4);
+
+        $instruction = new TestAssemblyInstruction();
+        $instruction->setSimulator($simulator);
+
+        $parseAddress = new ReflectionMethod($instruction, "parseAddress");
+
+        $byte = [
+            "mod" => 0,
+            "reg" => 0,
+            "rm" => 0b100,
+        ];
+
+        $address = $parseAddress->invoke($instruction, $byte);
+        $this->assertEquals(48, $address->getAddress());
+        $this->assertEquals(5, $address->getDisplacement());
+    }
+
+    public function testParseAddressWithSibEbpInProtectedMode()
+    {
+        $simulator = $this->getMockSimulator(Simulator::PROTECTED_MODE);
+
+        $simulator->method('getRex')
+                  ->willReturn(0);
+
+        $simulator->method('getPrefix')
+                  ->willReturn(0);
+
+        $simulator->expects($this->once())
+                  ->method('readRegister')
+                  ->willReturn(69)
+                  ->with(Register::EBP);
+
+        $simulator->method('getInstructionPointer')
+                  ->willReturn(3);
+
+        $simulator->method('getCodeAtInstruction')
+                  ->willReturn("\x25");
+
+        $simulator->expects($this->never())
+                  ->method('getCodeBuffer');
+
+        $instruction = new TestAssemblyInstruction();
+        $instruction->setSimulator($simulator);
+
+        $parseAddress = new ReflectionMethod($instruction, "parseAddress");
+
+        $byte = [
+            "mod" => 1,
+            "reg" => 0,
+            "rm" => 0b100,
+        ];
+
+        $address = $parseAddress->invoke($instruction, $byte);
+        $this->assertEquals(73, $address->getAddress());
+        $this->assertEquals(1, $address->getDisplacement());
     }
 }

tests/Unit/Instructions/MovTest.php
@@ -165,7 +165,7 @@ class MovTest extends \PHPUnit\Framework\TestCase
                   });
 
         $simulator->method('getCodeBuffer')
-                  ->with(1, 1)
+                  ->with(2, 1)
                   ->willReturn("\x10");
 
         $simulator->method('readRegister')