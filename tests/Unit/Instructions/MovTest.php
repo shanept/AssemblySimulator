shanept/AssemblySimulator

commit 385077a794a3fb93f0db74b751c3b8738e180ab5

Improved the addressing mode parsers, especially SIB.
The SIB parser was quite poorly implemented initially. This commit
implements it far better, removing a lot of the code smell. We
also implement handling of two's complement within the addressing
mode parsers for SIB and RIP.

src/Instruction/Move.php
@@ -98,12 +98,20 @@ class Move extends AssemblyInstruction
         $rmExt = (bool) ($rex & Simulator::REX_B);
 
         $opSize = $this->getOperandSize();
-        $rm = Register::getByCode($byte["rm"], $opSize, $rexSet, $rmExt);
 
         $reg = Register::getByCode($byte["reg"], $opSize, $rexSet, $regExt);
         $value = $sim->readRegister($reg);
 
-        $sim->writeRegister($rm, $value);
+        // We have a SIB byte.
+        if ($byte['rm'] & 0x4) {
+            $address = $this->parseAddress($byte);
+            $sim->advanceInstructionPointer($address->getDisplacement());
+            // This would write to a memory location. We will not be doing that.
+        } else {
+            $rm = Register::getByCode($byte["rm"], $opSize, $rexSet, $rmExt);
+
+            $sim->writeRegister($rm, $value);
+        }
 
         return true;
     }

tests/Unit/Instructions/MovTest.php
@@ -134,6 +134,47 @@ class MovTest extends \PHPUnit\Framework\TestCase
         $move->executeOperand89();
     }
 
+    /**
+     * We aren't looking for much here as we don't actually handle the value.
+     * We just want to ensure we move past the SIB value without error and
+     * return true.
+     */
+    public function testMov89OnSibValue()
+    {
+        $simulator = $this->getMockSimulator(Simulator::LONG_MODE);
+
+        // mov [rsp+0x10],rsi
+        // 0x48 0x89 0x74 0x24 0x10
+        $simulator->method('getRex')
+                  ->willReturn(0x48);
+
+        $simulator->method('getPrefix')
+                  ->willReturn(0);
+
+        $values = [
+            "\x10",
+            "\x24",
+            "\x74",
+        ];
+
+        $simulator->method('getCodeAtInstruction')
+                  ->willReturnCallback(function () use (&$values) {
+                      return array_pop($values);
+                  });
+
+        $simulator->method('readRegister')
+                  ->with(Register::RSI)
+                  ->willReturn(690);
+
+        $simulator->expects($this->exactly(3))
+                  ->method('advanceInstructionPointer');
+
+        $move = new Move();
+        $move->setSimulator($simulator);
+
+        $this->assertTrue($move->executeOperand89());
+    }
+
     public function testMov89OnRexRegisterValue()
     {
         $simulator = $this->getMockSimulator(Simulator::LONG_MODE);

src/Address/RipAddress.php
@@ -50,7 +50,23 @@ class RipAddress implements AddressInterface
 
     public function getAddress($offset = 0)
     {
-        return $this->rip + $this->address + $this->displacement + $offset;
+        $dispSize = $this->displacement;
+
+        // Handle two's compliment addresses.
+        if ($dispSize) {
+            $address = $this->address;
+
+            // This will generate the appropriate sized mask for the operation size.
+            $addrMask = (256 ** $dispSize) - 1;
+            $addrShift = (8 * $dispSize) - 1;
+
+            // If the most significant bit is set, we have a 2's complement.
+            if (($address >> $addrShift) & 1) {
+                $address = -(((~$address) & $addrMask) + 1);
+            }
+        }
+
+        return $this->rip + $address + $this->displacement + $offset;
     }
 
     public function getDisplacement()

src/Address/SibAddress.php
@@ -8,8 +8,6 @@ declare(strict_types=1);
 
 namespace shanept\AssemblySimulator\Address;
 
-use shanept\AssemblySimulator\Simulator;
-
 /**
  * Resolves SIB addresses.
  *
@@ -18,105 +16,74 @@ use shanept\AssemblySimulator\Simulator;
 class SibAddress implements AddressInterface
 {
     /**
-     * Stores the status of the REX bit when we got to this location, or empty
-     * on non-LONG modes.
+     * Stores the offset to the end of the instruction.
      *
      * @var int
      */
-    private $rex;
+    private $offset;
 
     /**
-     * Stores the opcode prefix for the current operation.
-     *
-     * @var int
-     */
-    private $prefix;
-
-    /**
-     * Stores a copy of the registers when the SIB byte was encountered.
+     * Stores the SIB byte.
      *
      * @var int[]
      */
-    private $registers;
+    private $sib;
 
     /**
-     * Stores the unparsed SIB byte.
-     *
-     * @var int
-     */
-    private $sibByte;
-
-    /**
-     * The displacement of the address in bytes.
+     * The displacement of the address from the SIB formula.
      *
      * @var int
      */
     private $displacement;
 
     /**
-     * Stores the offset to the end of the instruction.
+     * The size of the SIB byte address in bytes.
      *
      * @var int
      */
-    private $instructionOffset;
+    private $sibSize;
 
     /**
-     * @param int    $rex    The current REX bit.
-     * @param int    $pref   Current opcode prefixes.
-     * @param int[]  $regs   A copy of the registers.
-     * @param string $sib    The unparsed SIB byte.
-     * @param int    $disp   The displacement of the address.
-     * @param int    $offset The offset to the end of this instruction.
+     * @param int    $offset  The offset to the end of this instruction.
+     * @param int[]  $sib     The parsed SIB byte.
+     * @param int    $disp    The displacement of the address.
+     * @param int    $sibSize How many bytes the SIB address consumes.
      */
-    public function __construct($rex, $pref, $regs, $sib, $disp, $offset)
+    public function __construct($offset, $sib, $disp, $sibSize)
     {
-        $this->rex = $rex;
-        $this->prefix = $pref;
-        $this->registers = $regs;
-        $this->sibByte = ord($sib);
+        $this->offset = $offset;
+        $this->sib = $sib;
         $this->displacement = $disp;
-        $this->instructionOffset = $offset;
+        $this->sibSize = $sibSize;
     }
 
     public function getAddress($offset = 0)
     {
-        $scale = ($this->sibByte & 0b11000000) >> 6;
-        $index = ($this->sibByte & 0b00111000) >> 3;
-        $base = $this->sibByte & 0b00000111;
+        $scale = $this->sib['s'];
+        $index = $this->sib['i'];
+        $base = $this->sib['b'];
 
-        // Switch to LONG operands if specified by REX.
-        if ($this->rex & Simulator::REX_X) {
-            $index += 8;
+        $dispSize = $this->sibSize - 1;
+
+        // Handle two's compliment addresses.
+        if ($dispSize) {
+            $disp = $this->displacement;
+
+            // This will generate the appropriate sized mask for the operation size.
+            $dispMask = (256 ** $dispSize) - 1;
+            $dispShift = (8 * $dispSize) - 1;
+
+            // If the most significant bit is set, we have a 2's complement.
+            if (($disp >> $dispShift) & 1) {
+                $disp = -(((~$disp) & $dispMask) + 1);
+            }
         }
 
-        if ($this->rex & Simulator::REX_B) {
-            $base += 8;
-        }
-
-        /**
-         * Scale represents the multiplier for the index.
-         *
-         * If scale bit == 0b00, scale is 1.
-         * If scale bit == 0b01, scale is 2.
-         * If scale bit == 0b10, scale is 4.
-         * If scale bit == 0b11, scale is 8.
-         *
-         * Index and Base both refer to registers, in the same fashion as
-         * ModRM bytes.
-         */
-        $scale = 2 ** $scale;
-        $index = $this->registers[$index];
-        $base = $this->registers[$base];
-
-        return $scale * $index +
-            $base +
-            $this->displacement +
-            $offset +
-            $this->instructionOffset;
+        return ($scale * $index) + $base + $disp + $this->sibSize + $this->offset;
     }
 
     public function getDisplacement()
     {
-        return $this->displacement;
+        return $this->sibSize;
     }
 }

tests/Unit/Address/RipAddressTest.php
@@ -14,19 +14,31 @@ class RipAddressTest extends \PHPUnit\Framework\TestCase
         $this->assertInstanceOf(AddressInterface::class, $rip);
     }
 
-    public function testRipAddressResolvesCorrectly()
+    public static function ripAddressResolvesCorrectly()
     {
-        $rip = new RipAddress(100, 30, 8);
+        return [
+            // positive numbers
+            [100, 30, 8, 142],
+            [32, 50, 2, 88],
 
-        $this->assertEquals(142, $rip->getAddress());
+            // negative numbers
+            [95, 0xFFFFFFE2, 7, 76],
+            [17, 0xFFFFFFF0, 3, 8],
+        ];
     }
 
+    /**
+     * @dataProvider ripAddressResolvesCorrectly
+     */
+    public function testRipAddressResolvesCorrectly(
+        $ripPointer,
+        $address,
+        $offset,
+        $expected,
+    ) {
+        $rip = new RipAddress($ripPointer, $address, $offset);
 
-    public function testRipAddressResolvesCorrectlyWithOffset()
-    {
-        $rip = new RipAddress(200, 50, 6);
-
-        $this->assertEquals(300, $rip->getAddress(40));
+        $this->assertEquals($expected, $rip->getAddress());
     }
 
     public function testRipDisplacementIs32bit()

tests/Unit/Address/SibAddressTest.php
@@ -2,8 +2,6 @@
 
 namespace shanept\AssemblySimulatorTests\Unit\Address;
 
-use shanept\AssemblySimulator\Register;
-use shanept\AssemblySimulator\Simulator;
 use shanept\AssemblySimulator\Address\SibAddress;
 use shanept\AssemblySimulator\Address\AddressInterface;
 
@@ -11,125 +9,66 @@ class SibAddressTest extends \PHPUnit\Framework\TestCase
 {
     public function testImplements()
     {
-        $sib = new SibAddress(0, 0, [], '\x0', 0, 0);
+        $sib = new SibAddress(0, [], 0, 0);
 
         $this->assertInstanceOf(AddressInterface::class, $sib);
     }
 
-    public function testSibAddressResolvesCorrectly()
+    public static function sibAddressResolvesCorrectly()
     {
-        // mov [eax+ebx*4],ecx
-        // 89 0c 98
-        $registers = [
-            Register::EAX["offset"] => 1948657,
-            Register::ECX["offset"] => 0,
-            Register::EDX["offset"] => 0,
-            Register::EBX["offset"] => 635,
-            Register::ESP["offset"] => 0,
-            Register::EBP["offset"] => 0,
-            Register::ESI["offset"] => 0,
-            Register::EDI["offset"] => 0,
+        return [
+            // positive numbers
+            [8, 19457, 4634635, 9746, 0, 4800042],
+            [8, 525745, 634525, 242373, 30, 5082893],
+            [4, 34266, 6534335, 255, 23, 6671682],
+            [4, 194, 643635, 754425, 99, 1398940],
+            [2, 345, 54532, 9203454, 101, 9258782],
+            [2, 643, 65484, 9204853, 7, 9271635],
+            [1, 432, 234324, 92045437, 2222, 92282420],
+            [1, 6575, 85745, 9203254, 15, 9295594],
+
+            // negative numbers
+            [8, 19457, 4634635, 0xFFFFD9EE, 0, 4780550],
+            [8, 525745, 634525, 0xFFFC4D3B, 30, 4598147],
+            [4, 34266, 6534335, 0xFFFFFF01, 23, 6671172],
+            [4, 194, 643635, 0xFFFF7D07, 99, 610986],
+            [2, 345, 54532, 0xFFFFA462, 101, 31874],
+            [2, 643, 65484, 0xFFFF0DA0, 7, 4734],
+            [1, 432, 234324, 0xFFFE6423, 2222, 131546],
+            [1, 6575, 85745, 0xFFFFB09B, 15, 72015],
         ];
-
-        /**
-         * No rex. No prefix.
-         * Values pre-defined in EAX and EBX.
-         * SIB byte is \x98. (Scale: 4, Index: EBX, Base: EAX)
-         * Displacement is 4 bytes (32-bits)
-         */
-        $sib = new SibAddress(0, 0, $registers, "\x98", 4, 0);
-
-        // Expect (scale * index) + base + displacement
-        $expected =
-            4 * $registers[Register::EBX["offset"]] +
-            $registers[Register::EAX["offset"]] +
-            4;
-
-        $this->assertEquals($expected, $sib->getAddress());
     }
 
-    public function testSibAddressResolvesRexCorrectly()
-    {
-        // mov [rax+rbx*4],ecx
-        // 43 89 0c 98
-        $registers = [
-            Register::RAX["offset"] => 1948657,
-            Register::RCX["offset"] => 0,
-            Register::RDX["offset"] => 0,
-            Register::RBX["offset"] => 635,
-            Register::RSP["offset"] => 0,
-            Register::RBP["offset"] => 0,
-            Register::RSI["offset"] => 0,
-            Register::RDI["offset"] => 0,
-            Register::R8["offset"] => 429841241,
-            Register::R9["offset"] => 0,
-            Register::R10["offset"] => 0,
-            Register::R11["offset"] => 539,
-            Register::R12["offset"] => 0,
-            Register::R13["offset"] => 0,
-            Register::R14["offset"] => 0,
-            Register::R15["offset"] => 0,
+    /**
+     * @dataProvider sibAddressResolvesCorrectly
+     */
+    public function testSibAddressResolvesCorrectly(
+        $scale,
+        $index,
+        $base,
+        $displacement,
+        $instPointer,
+        $expected,
+    ) {
+        $sib = [
+            's' => $scale,
+            'i' => $index,
+            'b' => $base,
         ];
 
-        $rex = Simulator::REX_B | Simulator::REX_X;
-
-        /**
-         * REX_BX. No prefix.
-         * Values pre-defined in EAX and EBX.
-         * SIB byte is \x98. (Scale: 4, Index: EBX, Base: EAX)
-         * Displacement is 4 bytes (32-bits)
-         */
-        $sib = new SibAddress($rex, 0, $registers, "\x98", 4, 0);
-
-        // Expect (scale * index) + base + displacement
-        $expected =
-            4 * $registers[Register::R11["offset"]] +
-            $registers[Register::R8["offset"]] +
-            4;
+        $sib = new SibAddress($instPointer, $sib, $displacement, 5);
 
         $this->assertEquals($expected, $sib->getAddress());
-    }
-
-    public function testSibAddressResolvesCorrectlyWithOffset()
-    {
-        // mov [eax+ebx*4],ecx
-        // 89 0c 98
-        $registers = [
-            Register::EAX["offset"] => 1948657,
-            Register::ECX["offset"] => 0,
-            Register::EDX["offset"] => 0,
-            Register::EBX["offset"] => 635,
-            Register::ESP["offset"] => 0,
-            Register::EBP["offset"] => 0,
-            Register::ESI["offset"] => 0,
-            Register::EDI["offset"] => 0,
-        ];
-
-        /**
-         * No rex. No prefix.
-         * Values pre-defined in EAX and EBX.
-         * SIB byte is \x98. (Scale: 4, Index: EBX, Base: EAX)
-         * Displacement is 4 bytes (32-bits)
-         */
-        $sib = new SibAddress(0, 0, $registers, "\x98", 4, 0);
-
-        // Expect (scale * index) + base + displacement
-        $expected =
-            4 * $registers[Register::EBX["offset"]] +
-            $registers[Register::EAX["offset"]] +
-            4;
-
-        $this->assertEquals($expected + 40, $sib->getAddress(40));
     }
 
     public function testDisplacementReturnsIntFromConstruct()
     {
-        $sib0 = new SibAddress(0, 0, [], "\x0", 0, 0);
-        $sib8 = new SibAddress(0, 0, [], "\x0", 1, 0);
-        $sib32 = new SibAddress(0, 0, [], "\x0", 4, 0);
+        $sib0 = new SibAddress(0, [], 0, 1);
+        $sib8 = new SibAddress(0, [], 0, 2);
+        $sib32 = new SibAddress(0, [], 0, 5);
 
-        $this->assertEquals(0, $sib0->getDisplacement());
-        $this->assertEquals(1, $sib8->getDisplacement());
-        $this->assertEquals(4, $sib32->getDisplacement());
+        $this->assertEquals(1, $sib0->getDisplacement());
+        $this->assertEquals(2, $sib8->getDisplacement());
+        $this->assertEquals(5, $sib32->getDisplacement());
     }
 }

commit 27d31d949767a9a94f5214c8b32e9a358afc9459

Move Instruction SIB bug fix
Fixes bug introduced by commit 385077a for operand 89 where any
ModRM rm field of 4 or greater would be considered a SIB address.
The intended behaviour was for the field to be treated as a SIB
address only if the rm field equals 4. We have introduced a new
unit test for this bug as well.

src/Instruction/Move.php
@@ -97,7 +97,7 @@ class Move extends AssemblyInstruction
         $value = $sim->readRegister($reg);
 
         // We have a SIB byte.
-        if ($byte['rm'] & 0x4) {
+        if ($byte['rm'] === 0x4) {
             $address = $this->parseAddress($byte);
             $sim->advanceInstructionPointer($address->getDisplacement());
             // This would write to a memory location. We will not be doing that.
@@ -113,7 +113,7 @@ class Move extends AssemblyInstruction
     /**
      * Performs a MOV for a non-8-bit registry to ModRM byte.
      *
-     * Implements MOV reg,r/m for the opcode range \x8b.
+     * Implements MOV reg,r/m for the opcode \x8b.
      *
      * @return bool
      */

tests/Unit/Instructions/MovTest.php
@@ -186,6 +186,30 @@ class MovTest extends \PHPUnit\Framework\TestCase
         $this->assertTrue($move->executeOperand89());
     }
 
+    public function testMov89OnNonSibValue()
+    {
+        $simulator = $this->getMockSimulator(Simulator::LONG_MODE);
+
+        $simulator->method('getRex')
+                  ->willReturn(0x49);
+
+        $simulator->method('getCodeAtInstruction')
+                  ->willReturn("\xFD");
+
+        $simulator->method('readRegister')
+                  ->with(Register::RDI)
+                  ->willReturn(133);
+
+        $simulator->expects($this->once())
+                  ->method('writeRegister')
+                  ->with(Register::R13, 133);
+
+        $move = new Move();
+        $move->setSimulator($simulator);
+
+        $move->executeOperand89();
+    }
+
     public function testMov89OnRexRegisterValue()
     {
         $simulator = $this->getMockSimulator(Simulator::LONG_MODE);