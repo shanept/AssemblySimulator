shanept/AssemblySimulator

commit 06d29671d14cde3ca6cdbdbb8bb3a34b5b1c48c1

Added 8-bit operands for XOR and MOV. Tests to come.

src/Instruction/Move.php
@@ -25,7 +25,9 @@ class Move extends AssemblyInstruction
     public function register(): array
     {
         return [
+            0x88 => [&$this, 'executeOperand88'],
             0x89 => [&$this, 'executeOperand89'],
+            0x8A => [&$this, 'executeOperand8A'],
             0x8B => [&$this, 'executeOperand8B'],
             0xB8 => [&$this, 'executeOperandBx'],
             0xB9 => [&$this, 'executeOperandBx'],
@@ -38,6 +40,54 @@ class Move extends AssemblyInstruction
             0xC6 => [&$this, 'executeOperandC6'],
             0xC7 => [&$this, 'executeOperandC7'],
         ];
+    }
+
+    /**
+     * Performs a MOV for an 8-bit registry to ModRM byte.
+     *
+     * Implements MOV r/m8,reg8 for the opcode \x88.
+     * This is the alternate encoding of MOV \x8a.
+     */
+    public function executeOperand88(): bool
+    {
+        return $this->executeMovWithEncodingMr(Simulator::TYPE_BYTE);
+    }
+
+    /**
+     * Performs a MOV for a non-8-bit registry to ModRM byte.
+     *
+     * Implements MOV r/m,reg for the opcode \x89.
+     * This is the alternate encoding of MOV \x8b.
+     */
+    public function executeOperand89(): bool
+    {
+        $sim = $this->getSimulator();
+        $opSize = $this->getOperandSize();
+
+        return $this->executeMovWithEncodingMr($opSize);
+    }
+
+    /**
+     * Performs a MOV for an 8-bit registry to ModRM byte.
+     *
+     * Implements MOV reg,r/m for the opcode \x8a.
+     */
+    public function executeOperand8a(): bool
+    {
+        return $this->executeMovWithEncodingRm(Simualtor::TYPE_BYTE);
+    }
+
+    /**
+     * Performs a MOV for a non-8-bit registry to ModRM byte.
+     *
+     * Implements MOV reg,r/m for the opcode \x8b.
+     */
+    public function executeOperand8b(): bool
+    {
+        $sim = $this->getSimulator();
+        $opSize = $this->getOperandSize();
+
+        return $this->executeMovWithEncodingRm($opSize);
     }
 
     /**
@@ -66,85 +116,6 @@ class Move extends AssemblyInstruction
         $register = Register::getByCode($opcode, $opSize, $rexSet, $regExt);
 
         $sim->writeRegister($register, $value);
-
-        return true;
-    }
-
-    /**
-     * Performs a MOV for a non-8-bit registry to ModRM byte.
-     *
-     * Implements MOV r/m,reg for the opcode \x89.
-     * This is the alternate encoding of MOV \x8b.
-     */
-    public function executeOperand89(): bool
-    {
-        $sim = $this->getSimulator();
-        $sim->advanceInstructionPointer(1);
-
-        $byte = $this->parseModRmByte($sim->getCodeAtInstruction(1));
-        $sim->advanceInstructionPointer(1);
-
-        $rex = $sim->getRex();
-        $rexSet = (bool) ($rex & Simulator::REX);
-        $regExt = (bool) ($rex & Simulator::REX_R);
-        $rmExt = (bool) ($rex & Simulator::REX_B);
-
-        $opSize = $this->getOperandSize();
-
-        $reg = Register::getByCode($byte["reg"], $opSize, $rexSet, $regExt);
-        $value = $sim->readRegister($reg);
-
-        // We have an address
-        if (0b11 !== $byte['mod']) {
-            $address = $this->parseAddress($byte);
-            $sim->advanceInstructionPointer($address->getDisplacement());
-            // This would write to a memory location. We will not be doing that.
-        } else {
-            $rm = Register::getByCode($byte["rm"], $opSize, $rexSet, $rmExt);
-
-            $sim->writeRegister($rm, $value);
-        }
-
-        return true;
-    }
-
-    /**
-     * Performs a MOV for a non-8-bit registry to ModRM byte.
-     *
-     * Implements MOV reg,r/m for the opcode \x8b.
-     */
-    public function executeOperand8b(): bool
-    {
-        $sim = $this->getSimulator();
-        $sim->advanceInstructionPointer(1);
-
-        $byte = $this->parseModRmByte($sim->getCodeAtInstruction(1));
-        $sim->advanceInstructionPointer(1);
-
-        $rex = $sim->getRex();
-        $rexSet = (bool) ($rex & Simulator::REX);
-        $regExt = (bool) ($rex & Simulator::REX_R);
-
-        $opSize = $this->getOperandSize();
-        $reg = Register::getByCode($byte["reg"], $opSize, $rexSet, $regExt);
-
-        /**
-         * Depending on the value of the mod bit, we may be moving from a
-         * register or a memory address. If our ModRM mod field is 0b11, we
-         * source our value from the register. Otherwise, we load it from a
-         * memory address.
-         */
-        if (0b11 === $byte["mod"]) {
-            $rmExt = (bool) ($rex & Simulator::REX_B);
-            $rm = Register::getByCode($byte["rm"], $opSize, $rexSet, $rmExt);
-            $value = $sim->readRegister($rm);
-        } else {
-            $address = $this->parseAddress($byte);
-            $sim->advanceInstructionPointer($address->getDisplacement());
-            $value = $address->getAddress();
-        }
-
-        $sim->writeRegister($reg, $value);
 
         return true;
     }
@@ -231,6 +202,81 @@ class Move extends AssemblyInstruction
             $sim->writeRegister($rm, $value);
         }
         // This is where we *should* save to a memory location. We won't.
+
+        return true;
+    }
+
+    /**
+     * Provides the functionality for operands 88 and 89. They simply dictate
+     * the operand size, this function does the rest.
+     * Parameter encoding is "MR".
+     */
+    private function executeMovWithEncodingMr($opSize): bool
+    {
+        $sim = $this->getSimulator();
+        $sim->advanceInstructionPointer(1);
+
+        $byte = $this->parseModRmByte($sim->getCodeAtInstruction(1));
+        $sim->advanceInstructionPointer(1);
+
+        $rex = $sim->getRex();
+        $rexSet = (bool) ($rex & Simulator::REX);
+        $regExt = (bool) ($rex & Simulator::REX_R);
+        $rmExt = (bool) ($rex & Simulator::REX_B);
+
+        $reg = Register::getByCode($byte["reg"], $opSize, $rexSet, $regExt);
+        $value = $sim->readRegister($reg);
+
+        // We have an address
+        if (0b11 !== $byte['mod']) {
+            $address = $this->parseAddress($byte);
+            $sim->advanceInstructionPointer($address->getDisplacement());
+            // This would write to a memory location. We will not be doing that.
+        } else {
+            $rm = Register::getByCode($byte["rm"], $opSize, $rexSet, $rmExt);
+
+            $sim->writeRegister($rm, $value);
+        }
+
+        return true;
+    }
+
+    /**
+     * Provides the functionality for operands 88 and 89. They simply dictate
+     * the operand size, this function does the rest.
+     * Parameter encoding is "RM".
+     */
+    private function executeMovWithEncodingRm($opSize): bool
+    {
+        $sim = $this->getSimulator();
+        $sim->advanceInstructionPointer(1);
+
+        $byte = $this->parseModRmByte($sim->getCodeAtInstruction(1));
+        $sim->advanceInstructionPointer(1);
+
+        $rex = $sim->getRex();
+        $rexSet = (bool) ($rex & Simulator::REX);
+        $regExt = (bool) ($rex & Simulator::REX_R);
+
+        $reg = Register::getByCode($byte["reg"], $opSize, $rexSet, $regExt);
+
+        /**
+         * Depending on the value of the mod bit, we may be moving from a
+         * register or a memory address. If our ModRM mod field is 0b11, we
+         * source our value from the register. Otherwise, we load it from a
+         * memory address.
+         */
+        if (0b11 === $byte["mod"]) {
+            $rmExt = (bool) ($rex & Simulator::REX_B);
+            $rm = Register::getByCode($byte["rm"], $opSize, $rexSet, $rmExt);
+            $value = $sim->readRegister($rm);
+        } else {
+            $address = $this->parseAddress($byte);
+            $sim->advanceInstructionPointer($address->getDisplacement());
+            $value = $address->getAddress();
+        }
+
+        $sim->writeRegister($reg, $value);
 
         return true;
     }

src/Instruction/ExclusiveOr.php
@@ -26,12 +26,60 @@ class ExclusiveOr extends AssemblyInstruction
     public function register(): array
     {
         return [
+            0x30 => [&$this, 'executeOperand30'],
             0x31 => [&$this, 'executeOperand31'],
+            0x32 => [&$this, 'executeOperand32'],
             0x33 => [&$this, 'executeOperand33'],
         ];
     }
 
-    private function preExecXorModRM(): array
+    /**
+     * Performs an xor operation on two registers.
+     *
+     * Implements XOR source8,dest8 for the xor opcode \x30. Result stored in rm.
+     */
+    public function executeOperand30(): bool
+    {
+        return $this->executeXorWithEncodingMr(Simulator::TYPE_BYTE);
+    }
+
+    /**
+     * Performs an xor operation on two registers.
+     *
+     * Implements XOR source,dest for the xor opcode \x31. Result stored in rm.
+     */
+    public function executeOperand31(): bool
+    {
+        $sim = $this->getSimulator();
+        $opSize = $this->getOperandSize();
+
+        return $this->executeXorWithEncodingMr($opSize);
+    }
+
+    /**
+     * Performs an xor operation on two registers.
+     *
+     * Implements XOR source8,dest8 for the xor opcode \x32. Result stored in reg.
+     */
+    public function executeOperand32(): bool
+    {
+        return $this->executeXorWithEncodingRm(Simulator::TYPE_BYTE);
+    }
+
+    /**
+     * Performs an xor operation on two registers.
+     *
+     * Implements XOR source,dest for the xor opcode \x33. Result stored in reg.
+     */
+    public function executeOperand33(): bool
+    {
+        $sim = $this->getSimulator();
+        $opSize = $this->getOperandSize();
+
+        return $this->executeXorWithEncodingRm($opSize);
+    }
+
+    private function preExecXorModRM($opSize): array
     {
         $sim = $this->getSimulator();
         $sim->advanceInstructionPointer(1);
@@ -54,7 +102,6 @@ class ExclusiveOr extends AssemblyInstruction
         $regExt = (bool) ($rex & Simulator::REX_R);
         $rmExt = (bool) ($rex & Simulator::REX_B);
 
-        $opSize = $this->getOperandSize();
         $reg = Register::getByCode($byte["reg"], $opSize, $rexSet, $regExt);
         $rm = Register::getByCode($byte["rm"], $opSize, $rexSet, $rmExt);
 
@@ -64,16 +111,11 @@ class ExclusiveOr extends AssemblyInstruction
         ];
     }
 
-    /**
-     * Performs an xor operation on two registers.
-     *
-     * Implements XOR source,dest for the xor opcode \x31. Result stored in rm.
-     */
-    public function executeOperand31(): bool
+    private function executeXorWithEncodingMr($opSize)
     {
         $sim = $this->getSimulator();
 
-        $modrm = $this->preExecXorModRM();
+        $modrm = $this->preExecXorModRM($opSize);
 
         $regVal = $sim->readRegister($modrm['reg']);
         $rmVal = $sim->readRegister($modrm['rm']);
@@ -89,16 +131,11 @@ class ExclusiveOr extends AssemblyInstruction
         return true;
     }
 
-    /**
-     * Performs an xor operation on two registers.
-     *
-     * Implements XOR source,dest for the xor opcode \x33. Result stored in reg.
-     */
-    public function executeOperand33(): bool
+    private function executeXorWithEncodingRm($opSize)
     {
         $sim = $this->getSimulator();
 
-        $modrm = $this->preExecXorModRM();
+        $modrm = $this->preExecXorModRM($opSize);
 
         $regVal = $sim->readRegister($modrm['reg']);
         $rmVal = $sim->readRegister($modrm['rm']);

commit a3519b5e135ae625a898e0c9cfae0b7de8ae2403

Fixed typo introduced by 06d2967

src/Instruction/Move.php
@@ -74,7 +74,7 @@ class Move extends AssemblyInstruction
      */
     public function executeOperand8a(): bool
     {
-        return $this->executeMovWithEncodingRm(Simualtor::TYPE_BYTE);
+        return $this->executeMovWithEncodingRm(Simulator::TYPE_BYTE);
     }
 
     /**